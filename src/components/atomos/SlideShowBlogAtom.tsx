c7d7f6d3439abe1410206465268f8211aeeef6df
Dev Felipe Nunes <felipe.oliveira@stellabarros.com.br>
2023-02-14 14:13:24 -0300

    t

--- src/components/atomos/SlideShowBlogAtom.tsx
+++ src/components/atomos/SlideShowBlogAtom.tsx
@@ -1,16 +1,13 @@
-import React from "react";
 import "../../index.css";
-
-import { Slide } from "react-slideshow-image";
-
 import "react-slideshow-image/dist/styles.css";
 
+import { TextAtom } from "./TextAtom";
+import { Slide } from "react-slideshow-image";
+import { BackgroundImageAtom } from "./BackgroundImageAtom";
 import { minorCardBlog } from "../../constants";
 
-import { TextAtom } from "./TextAtom";
-import { BackgroundImageAtom } from "./BackgroundImageAtom";
-
 export const SlideShowBlogAtom = () => {
+  console.log(minorCardBlog);
   return (
     <div
       className={
